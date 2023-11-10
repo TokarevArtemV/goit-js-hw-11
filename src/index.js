f6410e57fe2898732ccca12bbdd4392fbd3155fc
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-11-10 13:03:14 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import { refs } from './js/refs';
-import { printImage, loadMoreImages } from './js/searchImage';
-
+import { printImage } from './js/searchImage';
 
 refs.formEl.addEventListener('submit', printImage);
-refs.buttonLoadMoreEl.addEventListener('click', loadMoreImages);
-
-
